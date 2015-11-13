431fe13ee7b56675ba308871a4d1ab0bf5dcc9af
Ciszq <ciszqq@gmail.com>
2015-11-13 21:44:33 +0100

    Add simple data normalization

--- SVMClassifier/src/HeartBeatClassifier/HeartBeatClassifier.py
+++ SVMClassifier/src/HeartBeatClassifier/HeartBeatClassifier.py
@@ -1,5 +1,6 @@
-
 from SVMClassifier import *
+from sklearn.preprocessing import normalize
+
 
 class HeartBeatClassifier(object):
     def __init__(self):
@@ -8,5 +9,14 @@ class HeartBeatClassifier(object):
 
     def classify(self, data):
         number_of_clusters = data["k"]
+        normalizedQrsComplexData = __convertToQRSComplexData__(data)
+ #       gmeans = g_meand.ClusterData(normalizedQrsComplexData)
+        svm_classifier.predict(gmeans)
+
+
+    def __convertToQRSComplexData__(self,data):
+        normalizedData = dict((k, normalize(v[:,np.newaxis], axis=0).ravel()) for (k,v) in data.items())
+        return [QrsComplexData(simpleData["gd"]) for simpleData in normalizedData] #implement others
+
 
 
