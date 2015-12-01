bc3f88ca08b97c1b53904f219e7c7c6495482003
lukasz.dudek <lukasz.j.dudek@uj.edu.pl>
2015-12-01 23:02:36 +0100

    Added imports and begun implementation.

--- GMeans/gmeans.py
+++ GMeans/gmeans.py
@@ -1,4 +1,6 @@
-from SVMClassifier.src.HeartBeatClassifier.QRSData import QRS_DATA
+# from SVMClassifier.src.HeartBeatClassifier.QRSData import QRS_DATA
+from anderson_darling_test import AndersonDarlingTest
+from scipy.cluster.vq import kmeans2
 import logging
 
 class GMeans(object):
@@ -7,12 +9,24 @@ class GMeans(object):
 
     def clusterData(self, qrs_complexes, max_k):
         self.logger.debug('In clusterData')
+        data = self.qrsToGslVectors(qrs_complexes)
+        qrs_centroids = kmeans2(data, max_k, iter=100)
+        # :TODO: Check how kmeans2 beahaves and what input data it requires.
+        return qrs_centroids
 
-    def qrsToGslVectors(self):
+    def anderson_darling_test(self, data, alpha):
+        # To tylko wywolanie metody z zewnetrznego modulu.
+        self.logger.debug("In anderson_darling_test")
+        AndersonDarlingTest(data, alpha)
+
+    def qrsToGslVectors(self, qrs_complexes):
+        # Ta metoda w tym momencie nie ma zadnego sensu
+        # :TODO: This method might be used to prepare data for the kmeans implementation that we'll use.
+        self.logger.debug('In qrsToGslVectors')
         pass
 
+
 def main():
-    # gmeans_logger = logging.getLogger(__name__)
     pass
 
 
