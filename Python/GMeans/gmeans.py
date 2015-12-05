c3199b8424c704d894ae8ed1cbff51d38de17568
LJBD <restinginpeace@interia.pl>
2015-12-05 13:04:45 +0100

    Added a piece of documentation for GMeans and a directory for the raport.

--- Python/GMeans/gmeans.py
+++ Python/GMeans/gmeans.py
@@ -1,15 +1,16 @@
-# from SVMClassifier.src.HeartBeatClassifier.QRSData import QRS_DATA
+# from SVMClassifier.HeartBeatClassifier.QRSData import QRS_DATA
 from anderson_darling_test import AndersonDarlingTest
 from scipy.cluster.vq import kmeans2
 import logging
+
 
 class GMeans(object):
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
-    def clusterData(self, qrs_complexes, max_k):
+    def cluster_data(self, qrs_complexes, max_k):
         self.logger.debug('In clusterData')
-        data = self.qrsToGslVectors(qrs_complexes)
+        data = self.qrs_to_gsl_vectors(qrs_complexes)
         qrs_centroids = kmeans2(data, max_k, iter=100)
         # :TODO: Check how kmeans2 beahaves and what input data it requires.
         return qrs_centroids
@@ -19,11 +20,11 @@ class GMeans(object):
         self.logger.debug("In anderson_darling_test")
         AndersonDarlingTest(data, alpha)
 
-    def qrsToGslVectors(self, qrs_complexes):
+    def qrs_to_gsl_vectors(self, qrs_complexes):
         # Ta metoda w tym momencie nie ma zadnego sensu
         # :TODO: This method might be used to prepare data for the kmeans implementation that we'll use.
         self.logger.debug('In qrsToGslVectors')
-        pass
+        return []
 
 
 def main():
