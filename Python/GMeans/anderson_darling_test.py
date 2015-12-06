3be201b2ccd89545584bdb82cb32ce46db53ba65
LJBD <restinginpeace@interia.pl>
2015-12-06 23:45:04 +0100

    Implementation of real G-means in progress.

--- Python/GMeans/anderson_darling_test.py
+++ Python/GMeans/anderson_darling_test.py
@@ -8,14 +8,10 @@ revisions
 license
            Citation requested when using this work in research.
 """
-import scipy.stats as stat
+from scipy import stats
 from numpy import mean, var
-# you may use the already posted version of mean and variance Python routines.
-from math import *
+from math import sqrt, log
 from matplotlib import pyplot
-
-
-pnorm = stat.norm.cdf
 
 
 def adstatistic(X):
@@ -24,16 +20,16 @@ def adstatistic(X):
     """
     n = len(X)
     Y = X[:]
-    ybar = mean(Y)
-    yvar = var(Y)
-    ysd = sqrt(yvar)
-    Y = [(y - ybar)/ysd for y in Y]
+    y_bar = mean(Y)
+    y_var = var(Y)
+    ysd = sqrt(y_var)
+    Y = [(y - y_bar)/ysd for y in Y]
     A2 = -n
     S = 0.0
     Y.sort()  # don't forget this!!!
     for i, y in enumerate(Y):
         j = i+1
-        p = pnorm(y)
+        p = stats.norm.cdf(y)
         q = 1 - p
         S += (j+j - 1)*log(p)+ (2 *(n-j)+1)* log(q)
     A2 -= S/n
@@ -44,23 +40,23 @@ def adstatistic(X):
 
 def AndersonDarlingTest(X, alpha=0.0001):
     alphas = [0.10, 0.05,  0.025, 0.01, 0.0001]
-    critvalue = [0.632, 0.751, 0.870, 1.029, 1.8692]
+    critical_value = [0.632, 0.751, 0.870, 1.029, 1.8692]
     try:
         for i, a in enumerate(alphas):
             if abs(alpha - a) < 1.0e-4:
-                crit = critvalue[i]
+                crit = critical_value[i]
                 teststat = adstatistic(X)
-                print 'Test stat: %s, crit: %s' % (teststat, crit)
+                print 'Anderson-Darling test stat: %s, crit: %s' % (teststat, crit)
                 return teststat < crit
     except:
-       raise Exception("Signifance level not in range")
+        raise Exception("Significance level not in range")
     return None
 
 
 if __name__ == "__main__":
-    print pnorm(3.0)
+    print stats.norm.cdf(3.0)
     n = 10
-    data_x = [stat.norm.rvs() for i in range(n)]
+    data_x = [stats.norm.rvs() for i in range(n)]
     pyplot.plot(data_x, 'ro')
-    print 'TEST VERDICT:', AndersonDarlingTest(data_x)
+    print 'ANDERSON-DARLING TEST VERDICT:', AndersonDarlingTest(data_x)
     pyplot.show()
--- Python/GMeans/gmeans.py
+++ Python/GMeans/gmeans.py
@@ -2,25 +2,91 @@
 from anderson_darling_test import AndersonDarlingTest
 from scipy.cluster.vq import kmeans2
 from scipy.stats import multivariate_normal
+import numpy
 import logging
 
 
 class GMeans(object):
-    def __init__(self):
+    def __init__(self, log_level='INFO'):
         self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(log_level)
         # self.dummy_data = QRSData()
 
     def cluster_data(self, qrs_complexes, max_k=50):
         self.logger.debug('In clusterData')
-        data = self.qrs_conversion(qrs_complexes)
-        qrs_centroids = kmeans2(data, max_k, iter=100)
+        self.qrs_data = self.qrs_conversion(qrs_complexes)
+
+        ########################################
+        # Start the algorithm
+        ########################################
+        initial_centroid = calculate_mean(self.qrs_data)
+        k = 1
+        # minit='matrix' daje to, ze moge podac macierz poczatkowych centroidow jako k
+        qrs_centroids, labels_for_data = kmeans2(self.qrs_data, k=[initial_centroid], minit='matrix', iter=100)
         # :TODO: Check how kmeans2 beahaves and what input data it requires.
+        self.logger.debug('First k-means resulted in centroids: %s\n\tlabels: %s' % (qrs_centroids, labels_for_data))
+
+        self.labels_dict = {}
+        for i, centroid_number in enumerate(labels_for_data):
+            self.labels_dict[i] = centroid_number
+
+        while k < max_k:
+            kept_centroids = []
+            for centroid_index, centroid in enumerate(qrs_centroids):
+                data_for_centroid = get_data_for_centroid(centroid_index, labels_for_data)
+                test_outcome, new_centroids, new_labels_for_data = self.run_test(centroid, data_for_centroid)
+                if test_outcome:
+                    kept_centroids.append(centroid)
+                else:
+                    for item in new_centroids:
+                        kept_centroids.append(item)
+                    for index in data_for_centroid:
+                        index = new_labels_for_data
+                    # :TODO: Add else, in which I will update the labels_for_centroid with the two new centros' indices!
+
+            if kept_centroids == qrs_centroids:
+                break
+        # :TODO: Finish this method!
+
         return qrs_centroids
 
-    def anderson_darling_test(self, data, alpha):
-        # To tylko wywolanie metody z zewnetrznego modulu.
+    def anderson_darling_test(self, data, alpha=0.0001):
         self.logger.debug("In anderson_darling_test")
         return AndersonDarlingTest(data, alpha)
+
+    def run_test(self, centroid, data_for_centroid):
+        self.logger.debug("In run_test")
+        child_centroids = self.get_child_centroids(centroid, data_for_centroid)
+        new_centroids, labels_for_data = kmeans2(data_for_centroid, k=child_centroids, iter=100, minit='matrix')
+        if len(new_centroids) == 2:
+            v = [new_centroids[0][i] - new_centroids[1][i] for i in xrange(len(new_centroids[0]))]
+            projected_data = self.project_data_on_v(data_for_centroid, v)
+            test_outcome = self.anderson_darling_test(projected_data)
+            return test_outcome, new_centroids, labels_for_data
+        else:
+            raise ValueError('Unexpected number of centroids.')
+
+    def project_data_on_v(self, data_for_centroid, v):
+        self.logger.debug('In project_data_on_v')
+        projected_data = []
+        norm_of_v = numpy.linalg.norm(v)
+        real_data = self.get_real_data_from_indices(data_for_centroid)
+        for data_vector in real_data:
+            dot_product = numpy.dot(v, data_vector)
+            projected_data.append(dot_product/norm_of_v)
+        return projected_data
+
+    def get_child_centroids(self, centroid, data_for_centroid):
+        self.logger.debug("In get_child_centroids")
+        real_data = self.get_real_data_from_indices(data_for_centroid)
+        mean = calculate_mean(real_data)
+        child_centroid_1 = [centroid[i] - mean[i]/100.0 for i in xrange(len(centroid))]
+        child_centroid_2 = [centroid[i] + mean[i]/100.0 for i in xrange(len(centroid))]
+        return [child_centroid_1, child_centroid_2]
+
+    def get_real_data_from_indices(self, list_of_indices):
+        self.logger.debug("In get_real_data_from_indices")
+        return [self.qrs_data[i] for i in list_of_indices]
 
     def qrs_conversion(self, qrs_complexes):
         # Ta metoda w tym momencie nie ma zadnego sensu
@@ -28,13 +94,25 @@ class GMeans(object):
         self.logger.debug('In qrsToGslVectors')
         return qrs_complexes
 
-    def get_normal_distribution(self, n):
-        """
-        This method is be used to determine a value of a multidimensional normal probability density function.
-        :param n: required dimension of the PDF
-        """
-        self.normal_var = multivariate_normal(mean=[0, 0], cov=[[1, 0], [0, 1]])
-        self.normal_var.pdf([1, 0])
+    # def get_normal_distribution(self, n):
+    #     """
+    #     This method is be used to determine a value of a multidimensional normal probability density function.
+    #     :param n: required dimension of the PDF
+    #     """
+    #     self.normal_var = multivariate_normal(mean=[0, 0], cov=[[1, 0], [0, 1]])
+    #     self.normal_var.pdf([1, 0])
+
+
+def calculate_mean(input_list):
+    return [sum(i)/float(len(i)) for i in input_list]
+
+
+def get_data_for_centroid(centroid_index, labels_for_data):
+    data_for_centroid = []
+    for i in xrange(len(labels_for_data)):
+        if labels_for_data[i] == centroid_index:
+            data_for_centroid.append(i)
+    return labels_for_data
 
 
 def main():
