5f7ff9912d8f35f86ace619fccbcdb096d14cce5
Michał Ciszewski <ciszqq@gmail.com>
2015-12-06 21:36:38 +0100

    Add raw version file reading

--- Python/HeartBeatClassifier.py
+++ Python/HeartBeatClassifier.py
@@ -1,12 +1,29 @@
 
 from SVMClassifier import *
+from sklearn.preprocessing import normalize
+import os
 
 class HeartBeatClassifier(object):
     def __init__(self):
         self.svm_classifier = SVMClassifier()
-        # self g_means = GMeans()
+        self.g_means = GMeans()
 
-    def classify(self, data):
-        number_of_clusters = data["k"]
+    def classify(self):
+        number_of_clusters = 2;
+        data = __getQrsComplexDataFromFile('ConvertedQRSRawData.txt')
+        normalizedQrsComplexData = __convertToQrsComplexData__(data)
+        gmeans = g_means.ClusterData(normalizedQrsComplexData)
+        svm_classifier.predict(gmeans)
+        
+    def __convertToQrsComplexData__(self, data):
+        normalizedData = dict((k, normalize(v[:,np.newaxis], axis= 0).ravel()) for (k,v) in data.items())
+        #return 
+        
+    def __getQrsComplexDataFromFile(self, path):
+        dir = os.path.dirname(__file__)
+        filename = os.path.join(dir, path)
+        print filename
+        with open(filename, "r") as ins:
+            return [QRSData(line.split()) for line in ins]
 
 
