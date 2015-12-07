0df5d8243c8ba069f7e4a3df0d40bc9ddd24f1f9
lukasz.dudek <lukasz.j.dudek@uj.edu.pl>
2015-12-07 10:48:35 +0100

    Trying to get random data to test gmeans.

--- Python/GMeans/gmeans.py
+++ Python/GMeans/gmeans.py
@@ -1,9 +1,8 @@
-# from SVMClassifier.QRSData import QRS_DATA
 from anderson_darling_test import AndersonDarlingTest
 from scipy.cluster.vq import kmeans2
-from scipy.stats import multivariate_normal
 import numpy
 import logging
+from matplotlib import pyplot
 
 
 class GMeans(object):
@@ -33,7 +32,7 @@ class GMeans(object):
         while k < max_k:
             kept_centroids = []
             for centroid_index, centroid in enumerate(qrs_centroids):
-                data_for_centroid = get_data_for_centroid(centroid_index, labels_for_data)
+                data_for_centroid = self.get_data_for_centroid(centroid_index)
                 test_outcome, new_centroids, new_labels_for_data = self.run_test(centroid, data_for_centroid)
                 if test_outcome:
                     kept_centroids.append(centroid)
@@ -57,7 +56,8 @@ class GMeans(object):
     def run_test(self, centroid, data_for_centroid):
         self.logger.debug("In run_test")
         child_centroids = self.get_child_centroids(centroid, data_for_centroid)
-        new_centroids, labels_for_data = kmeans2(data_for_centroid, k=child_centroids, iter=100, minit='matrix')
+        real_data = self.get_real_data_from_indices(data_for_centroid)
+        new_centroids, labels_for_data = kmeans2(real_data, k=child_centroids, iter=100, minit='matrix')
         if len(new_centroids) == 2:
             v = [new_centroids[0][i] - new_centroids[1][i] for i in xrange(len(new_centroids[0]))]
             projected_data = self.project_data_on_v(data_for_centroid, v)
@@ -84,15 +84,23 @@ class GMeans(object):
         child_centroid_2 = [centroid[i] + mean[i]/100.0 for i in xrange(len(centroid))]
         return [child_centroid_1, child_centroid_2]
 
-    def get_real_data_from_indices(self, list_of_indices):
+    def get_real_data_from_indices(self, dict_of_indices):
         self.logger.debug("In get_real_data_from_indices")
-        return [self.qrs_data[i] for i in list_of_indices]
+        return [self.qrs_data[i] for i in dict_of_indices.keys()]
 
     def qrs_conversion(self, qrs_complexes):
         # Ta metoda w tym momencie nie ma zadnego sensu
         # :TODO: This method might be used to prepare data for the kmeans implementation that we'll use.
         self.logger.debug('In qrsToGslVectors')
         return qrs_complexes
+
+    def get_data_for_centroid(self, centroid_index):
+        self.logger.debug("In get_data_for_centroid")
+        data_for_centroid = {}
+        for key in self.labels_dict.keys():
+            if self.labels_dict[key] == centroid_index:
+                data_for_centroid[key] = centroid_index
+        return data_for_centroid
 
     # def get_normal_distribution(self, n):
     #     """
@@ -107,16 +115,23 @@ def calculate_mean(input_list):
     return [sum(i)/float(len(i)) for i in input_list]
 
 
-def get_data_for_centroid(centroid_index, labels_for_data):
-    data_for_centroid = []
-    for i in xrange(len(labels_for_data)):
-        if labels_for_data[i] == centroid_index:
-            data_for_centroid.append(i)
-    return labels_for_data
-
-
 def main():
-    pass
+    mean = [4.56, 5.234]
+    cov = [[1, 0],
+           [2, 5]]
+    data = numpy.random.multivariate_normal(mean=mean, cov=cov, size=(50, 1))
+    for row in data:
+        print row
+    x = [i[0][0] for i in data]
+    y = [i[0][1] for i in data]
+    print 'X ', x
+
+    nd = numpy.ndim(data)
+    print 'SIZES OF DATA: ', nd
+    pyplot.plot(x, y, 'ro')
+    pyplot.show()
+    g_means = GMeans(log_level='DEBUG')
+    g_means.cluster_data(data, max_k = 10)
 
 
 if __name__ == '__main__':
