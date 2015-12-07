5d2d0f909b0756a3f6a99beafbb7b42ba5114e98
Michał Ciszewski <ciszqq@gmail.com>
2015-12-07 13:07:29 +0100

    Raw version of data scaling
    It works, maybe

--- Python/HeartBeatClassifier.py
+++ Python/HeartBeatClassifier.py
@@ -1,7 +1,7 @@
 import SVMClassifier.SVMClassifier as svm
 from SVMClassifier.QRSData import QRSData
 import GMeans.gmeans as gm
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import MinMaxScaler
 import os
 import sys
 reload(sys)
@@ -15,22 +15,59 @@ class HeartBeatClassifier(object):
 
     def classify(self):
         number_of_clusters = 2
-        data = self.getQrsComplexDataFromFile('ConvertedQRSRawData.txt')
-        normalizedQrsComplexData = self.convertToQrsComplexData(data)
-        gmeans = self.g_means.cluster_data(normalizedQrsComplexData)
+        normalizeddata = self.getQrsComplexDataFromFile('ConvertedQRSRawData.txt')
+        print normalizeddata[0]
+        print 'dupa'
+        gmeans = self.g_means.cluster_data(normalizeddata)
         self.svm_classifier.predict(gmeans)
-        
-    def convertToQrsComplexData(self, data):
-        normalized_data = dict((k, normalize(v[:,np.newaxis], axis= 0).ravel()) for (k,v) in data.items())
-        return normalized_data
         
     def getQrsComplexDataFromFile(self, path):
         dir = os.path.dirname(os.path.dirname(__file__))
         dir = os.path.join(dir, 'Sygnały z C++')
         filename = os.path.join(dir, path)
-        print filename
         with open(filename, "r") as ins:
-            return [QRSData(line.split()) for line in ins]
+            qrs_data_list = [QRSData([float(x) for x in line.split()]) for line in ins]
+            
+        min_max_scaler = MinMaxScaler()    
+        r_peak_values = [item.r_peak_value for item in qrs_data_list]
+        p_onset_values = [item.p_onset_val for item in qrs_data_list]
+        p_peak_values = [item.p_peak_val for item in qrs_data_list]
+        p_end_values = [item.p_end_val for item in qrs_data_list]
+        qrs_onset_values = [item.qrs_onset_val for item in qrs_data_list]
+        qrs_end_values = [item.qrs_end_val for item in qrs_data_list]
+        t_peak_values = [item.t_peak_val for item in qrs_data_list]
+        t_end_values = [item.t_end_val for item in qrs_data_list]
+        scaled_r_peak_values = min_max_scaler.fit_transform(r_peak_values)
+        scaled_p_onset_values = min_max_scaler.fit_transform(p_onset_values)
+        scaled_p_peak_values = min_max_scaler.fit_transform(p_peak_values)
+        scaled_p_end_values = min_max_scaler.fit_transform(p_end_values)
+        scaled_qrs_onset_values = min_max_scaler.fit_transform(qrs_onset_values)
+        scaled_qrs_end_values = min_max_scaler.fit_transform(qrs_end_values)
+        scaled_t_peak_values = min_max_scaler.fit_transform(t_peak_values)
+        scaled_t_end_values = min_max_scaler.fit_transform(t_end_values)
+        data = []
+        for i in xrange(len(qrs_data_list)):
+            tmp = QRSData([qrs_data_list[i].r_peak,\
+                        scaled_r_peak_values[i],\
+                        qrs_data_list[i].rr_pre_interval,\
+                        qrs_data_list[i].rr_post_interval,\
+                        qrs_data_list[i].p_onset,\
+                        scaled_p_onset_values[i],\
+                        qrs_data_list[i].p_peak,\
+                        scaled_p_peak_values[i],\
+                        qrs_data_list[i].p_end,\
+                        scaled_p_end_values[i],\
+                        qrs_data_list[i].qrs_onset,\
+                        scaled_qrs_onset_values[i],\
+                        qrs_data_list[i].qrs_end,\
+                        scaled_qrs_end_values[i],\
+                        qrs_data_list[i].t_peak,\
+                        scaled_t_peak_values[i],\
+                        qrs_data_list[i].t_end,\
+                        scaled_t_end_values[i]\
+                        ])
+            data.append(tmp)
+        return data
 
     def run(self):
         pass
