2f04179656b06b1382cde9e4cf247662f373b9ae
Michał Ciszewski <ciszqq@gmail.com>
2015-12-14 22:08:40 +0100

    Small refactor

--- Matlab/getFeaturesMatrixAndLabelsVector.m
+++ Matlab/getFeaturesMatrixAndLabelsVector.m
@@ -1,50 +1,13 @@
 function [X,Y] = getFeaturesMatrixAndLabelsVector(qrs)
 
-feature_number = 16;
+feature_number = 18;
 number_of_elements = length(qrs);
-X = zeros(number_of_elements, feature_number);
-
-% p_onest time and value
-X(:,1) = [qrs.p_onset];
-X(:,2) = [qrs.p_onset_val];
-
-% p_peak time and value
-X(:,3) = [qrs.p_peak];
-X(:,4) = [qrs.p_peak_val];
-
-% p_end time and value
-X(:,5) = [qrs.p_end];
-X(:,6) = [qrs.p_end_val];
-
-% qrs_onset time and value
-X(:,7) = [qrs.qrs_onset];
-X(:,8) = [qrs.qrs_onset_val];
-
-% qrs_end time and value
-X(:,9) = [qrs.qrs_end];
-X(:,10) = [qrs.qrs_end_val];
-
-% t_peak time and value
-X(:,11) = [qrs.t_peak];
-X(:,12) = [qrs.t_peak_val];
-
-% t_end time and value
-X(:,13) = [qrs.t_end];
-X(:,14) = [qrs.t_end_val];
-
-% rr_intervals
-X(:,15) = [qrs.rr_pre_interval];
-X(:,16) = [qrs.rr_post_interval];
-
-% Normalize
-% for i = 1:feature_number
-%     vec = X(:,i);
-%     vec = vec - mean(vec);
-%     vec = vec/std(vec);
-%     X(:,i) = vec;
-% end
+X(number_of_elements, feature_number)=0;
+for i = 1 : number_of_elements
+X(1,:) = ToVector(qrs(i));
+end
 
 Y = zeros(number_of_elements,1);
 for i = 1:number_of_elements
-    Y(i) = qrs(i).class_id; %class2id(qrs(i).class);
+    Y(i) = qrs(i).class_id; % to zawsze bedzie 2 !! Tak chyba nie powinno byæ , tutaj skrypt class2id powinien byc wywo³any
 end
