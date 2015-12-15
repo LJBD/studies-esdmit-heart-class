2c89190caa62d98aa806125df377c6295d814bec
Michał Ciszewski <ciszqq@gmail.com>
2015-12-15 20:23:00 +0100

    Refactor TrainModel method

--- Matlab/TrainModel.m
+++ Matlab/TrainModel.m
@@ -1,22 +1,21 @@
 function [ model ] = TrainModel()
-dataId = 101;
-
-folder = num2str(dataId)
-path('libsvm-windows-dlls/', path); 
-
+dataId = [101 119 201];
 directory = fileparts(pwd);
-QRSDataPath = fullfile(directory, 'ReferencyjneDane' , folder , 'ConvertedQRSRawData.txt')
-QRSClassIdPath = fullfile(directory, 'ReferencyjneDane' , folder , 'Class_IDs.txt')
+QRSDataMatrix = [];
+QRSClassIdVector = [];
+
+for i = 1 : length(dataId)
+folder = num2str(dataId(i));
+QRSDataPath = fullfile(directory, 'ReferencyjneDane' , folder , 'ConvertedQRSRawData.txt');
+QRSClassIdPath = fullfile(directory, 'ReferencyjneDane' , folder , 'Class_IDs.txt');
 formatSpec = '%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f';
 QRSSize = [18 inf];
-QRSDataMatrix = GetQRSFromFile(QRSDataPath, formatSpec, QRSSize);
-QRSClassIdVector = GetQRSFromFile(QRSClassIdPath, '%f', [inf]);
+QRSDataMatrix = [QRSDataMatrix GetQRSFromFile(QRSDataPath, formatSpec, QRSSize);];
+QRSClassIdVector = [QRSClassIdVector, GetQRSFromFile(QRSClassIdPath, '%f', [inf])'];
+end
+
 normalizedQRSComplexes = ConvertToNormalizedQRSComplexesWithId(QRSDataMatrix, QRSClassIdVector);
-
-% % create matrix of featrures X and vector of corresponding labels Y
-% [X,Y] = getFeaturesMatrixAndLabelsVector(normalizedQRSComplexes);
 Y = zeros(length(normalizedQRSComplexes),1);
-
 X = zeros(length(normalizedQRSComplexes),16);
 for i = 1:length(normalizedQRSComplexes)
 Y(i) = normalizedQRSComplexes(i).class_id;
