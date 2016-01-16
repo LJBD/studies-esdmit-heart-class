00fd20c805b0127d3c970dfc7dbfaef9f85ddfac
LJBD <restinginpeace@interia.pl>
2016-01-16 17:32:33 +0100

    [Python] Finished with Gmeans.

--- Python/GMeans/gmeans.py
+++ Python/GMeans/gmeans.py
@@ -2,7 +2,6 @@ from anderson_darling_test import AndersonDarlingTest
 from scipy.cluster.vq import kmeans2
 import numpy
 import logging
-from matplotlib import pyplot
 
 
 class GMeans(object):
@@ -18,52 +17,51 @@ class GMeans(object):
         self.k = 0
         # self.dummy_data = QRSData()
 
-    def cluster_data(self, qrs_complexes, max_k=50):
+    def cluster_data(self, qrs_complexes, max_k=50, alpha=0.0001):
         self.logger.debug('In clusterData')
         self.qrs_data = self.qrs_conversion(qrs_complexes)
-
-        ########################################
-        # Start the algorithm
-        ########################################
         initial_centroid = self.calculate_mean(self.qrs_data)
         print('INITIAL CENTROID: ', initial_centroid)
         self.centroids = numpy.array([initial_centroid])
         self.k = len(self.centroids)
 
         while self.k < max_k:
-            # this flag will inform us if anything was added to the centroids
             added_to_centroids_flag = False
             self.centroids, labels_for_data = kmeans2(self.qrs_data, k=numpy.array(self.centroids),
                                                       minit='matrix', iter=100)
+            self.k = len(self.centroids)
             # :TODO: Check how kmeans2 beahaves and what input data it requires.
             self.logger.debug('k-means resulted in centroids: %s' % self.centroids)
             self.set_proper_labels(labels_for_data)
 
             for centroid_index, centroid in enumerate(self.centroids):
                 data_for_centroid = self.get_data_dict_for_centroid(centroid_index)
-                test_outcome, new_centroids, new_labels_for_data = self.run_test(centroid, data_for_centroid)
+                test_outcome, new_centroids, new_labels_for_data = self.run_test(centroid, data_for_centroid, alpha)
                 if test_outcome:
+                    self.logger.debug('The test was positive: the old centroid stays.')
                     pass
                 else:
+                    self.logger.debug('The test was negative: new centroids will be added.')
                     added_to_centroids_flag = True
                     # :TODO: Add else, in which I will update the labels_for_centroid with the two new centros' indices!
                     self.update_after_centroid_addition(centroid_index, data_for_centroid, new_centroids,
                                                         new_labels_for_data)
 
             if added_to_centroids_flag is False:
-                self.logger.info('I\'ve added nothing, so I\' stopping the algorithm. K = %d' % self.k)
+                self.logger.info('GMEANS: I\'ve added nothing, so I\' stopping the algorithm. K = %d' % self.k)
                 break
             else:
                 pass
         # :TODO: Finish this method!
 
-        return self.centroids
+        return self.centroids, self.labels_dict
 
     def update_after_centroid_addition(self, centroid_index, data_for_centroid, new_centroids, new_labels_for_data):
+        self.logger.debug('In update_after_centroid_addition')
         # the first centroid gets the old index
         self.centroids[centroid_index] = new_centroids[0]
         # the second is appended at the end
-        self.centroids = numpy.concatenate(self.centroids, new_centroids[1])
+        self.centroids = numpy.append(self.centroids, [new_centroids[1]], axis=0)
         new_centroid_index = len(self.centroids) - 1
         for i, label in enumerate(new_labels_for_data):
             if label == 2:
@@ -72,9 +70,10 @@ class GMeans(object):
             else:
                 data_index_to_be_kept = list(data_for_centroid.keys())[i]
                 if self.labels_dict[data_index_to_be_kept] != centroid_index:
-                    raise ValueError('Mismatching index of the centroid for %d!' % data_index_to_be_kept)
+                    raise ValueError('Mismatching index of centroid for %d!' % data_index_to_be_kept)
 
     def set_proper_labels(self, labels_for_data):
+        self.logger.debug('In set_proper_labels')
         for i, centroid_number in enumerate(labels_for_data):
             self.labels_dict[i] = centroid_number
 
@@ -82,7 +81,7 @@ class GMeans(object):
         self.logger.debug("In anderson_darling_test")
         return AndersonDarlingTest(data, alpha)
 
-    def run_test(self, centroid, data_for_centroid):
+    def run_test(self, centroid, data_for_centroid, alpha=0.0001):
         self.logger.debug("In run_test")
         data_list = self.get_data_list_for_centroid(data_for_centroid)
         child_centroids = self.get_child_centroids(centroid, data_list)
@@ -90,7 +89,7 @@ class GMeans(object):
         if len(new_centroids) == 2:
             v = [new_centroids[0][i] - new_centroids[1][i] for i in range(len(new_centroids[0]))]
             projected_data = self.project_data_on_v(data_list, v)
-            test_outcome = self.anderson_darling_test(projected_data)
+            test_outcome = self.anderson_darling_test(projected_data, alpha)
             return test_outcome, new_centroids, labels_for_data
         else:
             raise ValueError('Unexpected number of centroids (not 2).')
@@ -156,17 +155,43 @@ def main():
     #        [2, 5]]
     # data = numpy.random.multivariate_normal(mean=mean, cov=cov, size=(50, 1))
     # data generation
-    data = numpy.vstack((numpy.random.rand(150, 2) + numpy.array([.5, .5]),
-                         numpy.random.rand(150, 2) + numpy.array([1, 1]),
-                         numpy.random.rand(150, 2)))
-    x = [i[0] for i in data]
-    y = [i[1] for i in data]
+    data_a = numpy.random.rand(150, 2) + numpy.array([.5, .5])
+    data_b = numpy.random.rand(150, 2) + numpy.array([1, 1])
+    data_c = numpy.random.rand(150, 2)
+    data = numpy.vstack((data_a, data_b, data_c))
 
-    pyplot.plot(x, y, 'ro')
+    fig = pyplot.figure()
+    axis = fig.add_subplot(111)
+    data_list = [data_a, data_b, data_c]
+    for i, d in enumerate(data_list):
+        x = [item[0] for item in d]
+        y = [item[1] for item in d]
+        axis.plot(x, y, 'o')
     pyplot.show()
     g_means = GMeans(log_level='DEBUG')
-    g_means.cluster_data(data, max_k=10)
+    centroids, labels = g_means.cluster_data(data, max_k=10, alpha=0.0001)
+    print('I GOT:')
+    print('CENTROIDS', centroids)
+    print('LABELS:', labels)
+    colors = ['r', 'g', 'b', 'c', 'm', 'k', 'y']
+    fig = pyplot.figure()
+    axis = fig.add_subplot(111)
+    axis.set_prop_cycle(cycler('color', colors))
+    big_data_list = []
+    for i, centroid in enumerate(centroids):
+        axis.plot(centroid[0], centroid[1], 'o')
+        big_data_list.append([])
+    for key in labels.keys():
+        big_data_list[labels[key]].append(data[i])
+
+    for list_for_one_centroid in big_data_list:
+        x = [item[0] for item in list_for_one_centroid]
+        y = [item[1] for item in list_for_one_centroid]
+        axis.plot(x, y)
+
+    pyplot.show()
 
 
 if __name__ == '__main__':
+    from matplotlib import pyplot, cycler
     main()
