b7d7a6551e596a1052f0c20e1864f3b5267898d8
Michał Ciszewski <ciszqq@gmail.com>
2016-01-16 14:54:54 +0100

    [Julia] Add svm predict to main function, create histogram for data from predict method

--- Julia/DataClassifierForPackage.jl
+++ Julia/DataClassifierForPackage.jl
@@ -12,8 +12,9 @@ function DataClassifierForPackage(dataId, referenceModel)
     
 
     #TODO TemporaryDate until modulw will be completed
-    #referenceModel = loadSvmModel(ascii(joinpath(pwd(),"SVMClassifier\\model_copy"))) #TODO Have problems in svm.jl line 197
-    
+    dataId = 101
+    path = string(pwd(),"\\SVMClassifier\\model_copy")
+    referenceModel = loadSvmModel("D:\\repo_heart\\Julia\\SVMClassifier\\model_copy") #TODO Have problems in svm.jl line 197
     #
     
     folder = string(dataId)
@@ -28,7 +29,11 @@ function DataClassifierForPackage(dataId, referenceModel)
     #create matrix of featrures X and vector of corresponding labels Y
 
     Y = QRSClassIdVector
-    X = QRSDataMatrix[:,3:18]
+    X = normalizedQRSComplexes[:,3:18] #THIS IS WRONG Class in file
+    qrs_vector = QRS_DATA[]
+    for i = 1: size(normalizedQRSComplexes,2)
+    push!(qrs_vector, QRS_DATA(normalizedQRSComplexes[2:19,i]))
+    end
     
     # Grouping TODO
     magicNumber =  0.0005
@@ -45,4 +50,14 @@ function DataClassifierForPackage(dataId, referenceModel)
     svm = SVMClassifier()
     svm.model = referenceModel
     predict(svm, qrs_vector)
+    
+    qrsIdAfterPrediction = []
+    for i = 1: size(normalizedQRSComplexes,2)
+        qrsIdAfterPrediction = [qrsIdAfterPrediction; qrs_vector[i].class_id]
+    end
+
+    p = plot(
+        x=qrsIdAfterPrediction,
+        Geom.histogram(bincount = 1),
+        Guide.title("Klasyfikacja zespołów QRS"))
 end
