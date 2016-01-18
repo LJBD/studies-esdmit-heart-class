eb725573e0d0a53d5cd8d28328aaefaf0c5a30f9
Mucha92 <kmucha@student.agh.edu.pl>
2016-01-18 21:32:48 +0100

    [Matlab] Added logging to file

--- Matlab/main.m
+++ Matlab/main.m
@@ -5,10 +5,15 @@ modelname = 'model111';
 packages = [101 111 117 119 201];
 
 model = ReadModel(strcat('../SVM_models/',modelname));
+fp = fopen( 'log', 'a' );
+fprintf( fp, datestr(datetime('now')));
+fprintf( fp, '\n-------------------------------------\n');
 
 for i=1:length(packages)
     tic;
     DataClassifierForPackage(packages(i), model, false)
     execTime = toc;
-    fprintf(strcat('Package: ',num2str(packages(i)),', execution time: ', num2str(execTime),'s\n'))
+    fprintf(fp, strcat('Package: ',num2str(packages(i)),', execution time: ', num2str(execTime),'s\n'));
 end
+
+fclose(fp);
