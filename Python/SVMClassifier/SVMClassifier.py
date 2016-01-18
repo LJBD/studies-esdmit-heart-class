3f3b29c6ddcfcbe859132af085b3e3705d8950fa
Mucha92 <kmucha@student.agh.edu.pl>
2016-01-18 13:03:28 +0100

    Second attepmt to make change in SVMClassifier

--- Python/SVMClassifier/SVMClassifier.py
+++ Python/SVMClassifier/SVMClassifier.py
@@ -1,8 +1,9 @@
 __author__ = 'Krystian'
+
 from svm import *
 
 
-class SVMClassifier(object):
+class SVMClassifier:
     def __init__(self):
         self.model = svm_model()
         self.model.free_sv = -1
@@ -13,62 +14,27 @@ class SVMClassifier(object):
     def createSvmVector(self, qrs_complex):
         node_list = []
 
-        node = svm_node(1, qrs_complex.p_onset)
-        node_list.append(node)
-
-        node = svm_node(2, qrs_complex.p_onset_val)
-        node_list.append(node)
-
-        node = svm_node(3, qrs_complex.p_peak)
-        node_list.append(node)
-
-        node = svm_node(4, qrs_complex.p_peak_val)
-        node_list.append(node)
-
-        node = svm_node(5, qrs_complex.p_end)
-        node_list.append(node)
-
-        node = svm_node(6, qrs_complex.p_end_val)
-        node_list.append(node)
-
-        node = svm_node(7, qrs_complex.qrs_onset)
-        node_list.append(node)
-
-        node = svm_node(8, qrs_complex.qrs_onset_val)
-        node_list.append(node)
-
-        node = svm_node(9, qrs_complex.qrs_end)
-        node_list.append(node)
-
-        node = svm_node(10, qrs_complex.qrs_end_val)
-        node_list.append(node)
-
-        node = svm_node(11, qrs_complex.t_peak)
-        node_list.append(node)
-
-        node = svm_node(12, qrs_complex.t_peak_val)
-        node_list.append(node)
-
-        node = svm_node(13, qrs_complex.t_end)
-        node_list.append(node)
-
-        node = svm_node(14, qrs_complex.t_end_val)
-        node_list.append(node)
-
-        node = svm_node(15, qrs_complex.rr_pre_interval)
-        node_list.append(node)
-
-        node = svm_node(16, qrs_complex.rr_post_interval)
-        node_list.append(node)
-
-        node = svm_node(-1, None)
-        node_list.append(node)
+        node_list.append(qrs_complex.p_onset)
+        node_list.append(qrs_complex.p_onset_val)
+        node_list.append(qrs_complex.p_peak)
+        node_list.append(qrs_complex.p_peak_val)
+        node_list.append(qrs_complex.p_end)
+        node_list.append(qrs_complex.p_end_val)
+        node_list.append(qrs_complex.qrs_onset)
+        node_list.append(qrs_complex.qrs_onset_val)
+        node_list.append(qrs_complex.qrs_end)
+        node_list.append(qrs_complex.qrs_end_val)
+        node_list.append(qrs_complex.t_peak)
+        node_list.append(qrs_complex.t_peak_val)
+        node_list.append(qrs_complex.t_end)
+        node_list.append(qrs_complex.t_end_val)
+        node_list.append(qrs_complex.rr_pre_interval)
+        node_list.append(qrs_complex.rr_post_interval)
 
         return node_list
 
 
     def predict(self, qrs_complexes):
-        #it = qrs_complexes[0]
         for i in range(0, qrs_complexes.__len__()):
             # Convert data
             x = self.createSvmVector(qrs_complexes[i])
@@ -76,3 +42,4 @@ class SVMClassifier(object):
             class_id = int(svm_predict(self.model, x))
             # Save results
             qrs_complexes[i].class_id = class_id
+            print (class_id)
