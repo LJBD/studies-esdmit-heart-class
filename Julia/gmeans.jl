b7a6b7f885164d4dc816ca422cf905b0bcac3443
LJBD <restinginpeace@interia.pl>
2016-01-19 14:15:29 +0100

    [Julia] Corrected a few things and removed calling for DEBUG level of logging from Julia.

--- Julia/gmeans.jl
+++ Julia/gmeans.jl
@@ -12,11 +12,11 @@ function Gmeans(normalizedQRSComplexes)
         qrs_vector=[qrs_vector; qrs.QRSData(normalizedQRSComplexes[:,i])]
     end
     debug("Converted QRS complexes to python QRS.")
-    gMeans = gmeans_base.GMeans("DEBUG")
-    centroids, lables = gMeans[:cluster_data](qrs_vector)
-    info(str("Finished python gMeans centroids size: ", size(centroids), " labels size: ", size(labels)))
+    gMeans = gmeans_base.GMeans()
+    centroids, labels = gMeans[:cluster_data](qrs_vector)
+    info("Finished python gMeans centroids size: ", size(centroids), " labels size: ", size(labels))
     c_idx = [];
-    numberOfGroups = length(lables)
+    numberOfGroups = length(labels)
     for i = 1:numberOfGroups
         c_idx = [c_idx;lables[i-1]];
     end
--- Julia/main.jl
+++ Julia/main.jl
@@ -6,7 +6,7 @@ end
 using Logging
 Logging.configure(filename="logfile.log")
 Logging.configure(level=DEBUG)
-dataId = 101
+dataId = 117
 path = string(dirname(pwd()),"/SVM_models/model101")
 include("DataClassifierForPackage.jl")
 referenceModel = loadSvmModel(path) #TODO Have problems in svm.jl line 197
