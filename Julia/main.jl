c515687456a3e6c2b2b2b86ef5f213c709868215
Mucha92 <kmucha@student.agh.edu.pl>
2016-01-19 18:10:40 +0100

    [Report] Added new data to tables

--- Julia/main.jl
+++ Julia/main.jl
@@ -6,7 +6,8 @@ end
 using Logging
 Logging.configure(filename="logfile.log")
 Logging.configure(level=DEBUG)
-dataId = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114, 115, 116, 117, 119, 121, 122, 123, 124, 200, 201, 202, 203, 205, 208, 209, 221, 222, 223, 230, 231, 232, 233, 234];
+#dataId = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114, 115, 116, 117, 119, 121, 122, 123, 124, 200, 201, 202, 203, 205, 208, 209, 221, 222, 223, 230, 231, 232, 233, 234];
+dataId = [210, 212, 213, 214, 215, 217, 219, 220];
 path = string(dirname(pwd()),"/SVM_models/model101")
 include("DataClassifierForPackage.jl")
 referenceModel = loadSvmModel(path) #TODO Have problems in svm.jl line 197
