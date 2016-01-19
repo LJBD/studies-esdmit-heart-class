520b32cf5a1487d6c4e63986400d8afb83dd2db8
Michał Ciszewski <ciszqq@gmail.com>
2016-01-19 18:46:40 +0100

    [Julia]Description for histogram

--- Julia/HistSvgGenerator.jl
+++ Julia/HistSvgGenerator.jl
@@ -2,13 +2,13 @@ include("GetQRSFromFile.jl")
 Pkg.add("StatsBase")
 using Gadfly
 using StatsBase
-dataId = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114, 115, 116, 117, 119, 121, 122, 123, 124, 200, 201, 202, 203, 205, 208, 209, 221, 222, 223, 230, 231, 232, 233, 234];
+dataId = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114, 115, 117, 119, 121, 122, 123, 124, 200, 201, 202, 203, 205, 208, 209, 221, 222, 223, 230, 231, 232, 233, 234];
 directory = dirname(pwd())
 for i = 1:length(dataId)
     QRSClassIdPath = joinpath(directory, "ReferencyjneDane" , string(dataId[i]) , "Class_IDs.txt")
     QRSClassIdVector = GetQRSFromFile(QRSClassIdPath, Float64);
     QRSClassIdVectorSorted = sort(union(QRSClassIdVector))
     clusterElements = counts(round(Int64,QRSClassIdVector), int(QRSClassIdVectorSorted[end]))
-    p = plot(y = clusterElements)
+    p = Gadfly.plot(y = clusterElements,Geom.bar, Scale.y_log10, Guide.xlabel("Numer klastra"), Guide.ylabel("Ilość reprezentantów"),Guide.title("Ilość reprezentantów poszczególnych klastrów"))
     draw(SVG(string(dataId[i],".svg"), 4inch, 3inch), p)
 end
