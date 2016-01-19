3e190315760b3af50c11a96e0cf76d547f14a6f0
Mucha92 <kmucha@student.agh.edu.pl>
2016-01-19 16:18:54 +0100

    [Matlab] Logging cluster no.

--- Matlab/main.m
+++ Matlab/main.m
@@ -2,18 +2,21 @@
 clear all
 
 modelname = 'model111';
-packages = [101 111 117 119 201];
+packages = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114, 115, 116, 117, 119, 121, 122, 123, 124, 200, 201, 202, 203, 205, 208, 209, 221, 222, 223, 230, 231, 232, 233, 234];
 
 model = ReadModel(strcat('../SVM_models/',modelname));
 fp = fopen( 'log', 'a' );
+fprintf( fp, '\n\n');
 fprintf( fp, datestr(datetime('now')));
 fprintf( fp, '\n-------------------------------------\n');
+fprintf(fp, 'Package\ttime\t\t\tClusters\n');
 
 for i=1:length(packages)
     tic;
-    DataClassifierForPackage(packages(i), model, false)
+    K = DataClassifierForPackage(packages(i), model, false);
     execTime = toc;
-    fprintf(fp, strcat('Package: ',num2str(packages(i)),', execution time: ', num2str(execTime),'s\n'));
+    fprintf(fp, strcat(num2str(packages(i)),'\t\t', num2str(execTime,'%f'),'\t\t',num2str(K)));
+    fprintf( fp, '\n');
 end
 
 fclose(fp);
