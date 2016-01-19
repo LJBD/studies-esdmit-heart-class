3771d32f0314b4f1b20b4e3e28f923d436691cec
Michał Ciszewski <ciszqq@gmail.com>
2016-01-19 17:30:39 +0100

    [Julia]Add Histogram generator

---
+++ Julia/HistSvgGenerator.jl
@@ -0,0 +1,14 @@
+include("GetQRSFromFile.jl")
+Pkg.add("StatsBase")
+using Gadfly
+using StatsBase
+dataId = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114, 115, 116, 117, 119, 121, 122, 123, 124, 200, 201, 202, 203, 205, 208, 209, 221, 222, 223, 230, 231, 232, 233, 234];
+directory = dirname(pwd())
+for i = 1:length(dataId)
+    QRSClassIdPath = joinpath(directory, "ReferencyjneDane" , string(dataId[i]) , "Class_IDs.txt")
+    QRSClassIdVector = GetQRSFromFile(QRSClassIdPath, Float64);
+    QRSClassIdVectorSorted = sort(union(QRSClassIdVector))
+    clusterElements = counts(round(Int64,QRSClassIdVector), int(QRSClassIdVectorSorted[end]))
+    p = plot(x=clusterElements, Geom.histogram(bincount=size(clusterElements,1)))
+    draw(SVG(string(dataId[i],".svg"), 4inch, 3inch), p)
+end
