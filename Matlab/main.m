d0d14bc8e6b8a896e726a9e213c100f37b4e51e5
Mucha92 <kmucha@student.agh.edu.pl>
2016-01-19 18:53:59 +0100

    Added files with results

--- Matlab/main.m
+++ Matlab/main.m
@@ -2,8 +2,8 @@
 clear all
 
 modelname = 'model111';
-%packages = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114, 115, 116, 117, 119, 121, 122, 123, 124, 200, 201, 202, 203, 205, 208, 209, 210, 212, 213, 214, 215, 217, 219, 220, 221, 222, 223, 230, 231, 232, 233, 234];
-packages = [210, 212, 213, 214, 215, 217, 219, 220];
+packages = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114, 115, 117, 119, 121, 122, 123, 124, 200, 201, 202, 203, 205, 208, 209, 210, 212, 213, 214, 215, 217, 219, 220, 221, 222, 223, 230, 231, 232, 233, 234];
+%packages = [210, 212, 213, 214, 215, 217, 219, 220];
 model = ReadModel(strcat('../SVM_models/',modelname));
 fp = fopen( 'log', 'a' );
 fprintf( fp, '\n\n');
@@ -15,7 +15,7 @@ for i=1:length(packages)
     tic;
     K = DataClassifierForPackage(packages(i), model, false);
     execTime = toc;
-    fprintf(fp, strcat(num2str(packages(i)),'\t\t', num2str(execTime,'%f'),'\t\t',num2str(K)));
+    fprintf(fp, strcat(num2str(packages(i)),'\t\t\t', num2str(execTime,'%f'),'\t\t',num2str(K)));
     fprintf( fp, '\n');
 end
 
